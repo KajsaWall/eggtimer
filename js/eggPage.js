55001e6b06b23c72790f81c9933ee995426d0e61
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-10-02 14:34:55 +0200

    User is now able to mark egg answers

--- js/eggPage.js
+++ js/eggPage.js
@@ -1,55 +1,69 @@
 "use strict";
 
 function renderEggPage() {
-
     document.querySelector("main").innerHTML = `
-    
     <div id="eggPage">
-
         <div id="eggSize">
             <p>Select size of your egg</p>
             <div>
-                <div class="s">
-                    <button>S</button>
-                    <p>"<50g"</p>
+                <div>
+                    <button id="s">S</button>
+                    <p><50g</p>
                 </div>
-                <div class="m">
-                    <button>M</button>
-                    <p>"60g"</p>
+                <div>
+                    <button id="m">M</button>
+                    <p>60g</p>
                 </div>
-                <div class="l">
-                    <button>L</button>
-                    <p>">70g"</p>
+                <div>
+                    <button id="l">L</button>
+                    <p>>70g</p>
                 </div>
             </div>
         </div>
-
         <div id="eggHardness">
             <p>Select hardness of your egg</p>
             <div>
-                <div class="soft">
+                <div>
                     <div class="eggImage">
                     </div>
-                    <button>Soft</button>
+                    <button id="soft">Soft</button>
                 </div>
-                <div class="medium">
+                <div>
                     <div class="eggImage">
                     </div>
-                    <button>Medium</button>
+                    <button id="medium">Medium</button>
                 </div>
-                <div class="soft">
+                <div>
                     <div class="eggImage">
                     </div>
-                    <button>Hard</button>
+                    <button id="hard">Hard</button>
                 </div>
             </div>
-
         </div>
-
     </div>
-    <button id=next>Next</button>
+    <button id="next">Next</button>
     `;
 
-    document.getElementById("next").addEventListener("click", (renderStartTimer));
+    const eggSizeButtons = document.querySelectorAll("#eggSize button");
+    const eggHardnessButtons = document.querySelectorAll("#eggHardness button");
 
+    eggSizeButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Remove "marked" class from all egg size buttons
+            eggSizeButtons.forEach(btn => btn.classList.remove("marked"));
+            // Add "marked" class to the clicked button
+            button.classList.add("marked");
+        });
+    });
+
+    eggHardnessButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Remove "marked" class from all egg hardness buttons
+            eggHardnessButtons.forEach(btn => btn.classList.remove("marked"));
+            // Add "marked" class to the clicked button
+            button.classList.add("marked");
+        });
+    });
+
+    document.getElementById("next").addEventListener("click", renderStartTimer);
 }
