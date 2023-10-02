46cb53814e8bce6d7821b4e16c1929f7ec6419c9
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-10-02 12:56:54 +0200

    Updates

--- js/homepage.js
+++ js/homepage.js
@@ -5,10 +5,17 @@ function renderHomePage () {
     document.querySelector("main").innerHTML = `
     
     <div id="homePage">
-        <h3>Let's get cooking!</h3>
-        <p>Put your egg into the boiling water!"
+        <div class=line>
+        </div>
+        <div id=startInfo>
+            <p>Before you begin, remember to place the egg in boiling water before starting the timer in the final step.</p>
+            <div>
+            </div>
+        </div>
+        <div class=line>
+        </div>
     </div>
-    <button id=next>Next</button>
+    <button id=next>Let's get started!</button>
     `;
 
     document.getElementById("next").addEventListener("click", renderEggPage());
--- js/startTimer.js
+++ js/startTimer.js
@@ -1,5 +1,25 @@
 "use strict";
 
 function renderStartTimer () {
+
+    document.querySelector("main").innerHTML = `
+    
+    <div id="startTimer">
+
+        <div id="bigEggImage>
+        </div>
+        <div id="timer">
+        </div>
+        <button id=start>Start</button>
+
+    </div>
+    `;
+
+    document.getElementById("start").addEventListener("click", stopTimer());
+
+}
+
+function stopTimer() {
+    document.getElementById("start").outerHTML = `<button id=start>Stop</button>`;
     
 }
