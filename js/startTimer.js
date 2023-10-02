21332f84edfd18715a28a329f0e0ec45b26add39
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-10-02 15:43:52 +0200

    Functional timer!

--- js/startTimer.js
+++ js/startTimer.js
@@ -1,9 +1,12 @@
 "use strict";
 
-function renderStartTimer () {
+let timerInterval; // Variable to store the interval ID
+let isTimerRunning = false; // Flag to track whether the timer is running or paused
+let startingMinutes = 0; // Initial starting time in minutes
+let remainingTime = 0; // Variable to store the remaining time
 
+function renderStartTimer() {
     document.querySelector("main").innerHTML = `
-    
     <div id="startTimer">
         <div id="bigEggImage"></div>
         <p id="timer"></p>
@@ -12,35 +15,44 @@ function renderStartTimer () {
     `;
 
     document.getElementById("start").addEventListener("click", toggleTimer);
-
 }
 
 function toggleTimer() {
-
-
-    if(document.getElementById("start").textContent === "Start") {
-        document.getElementById("start").textContent = "Stop"
-        //Timer pauses 
-    } else {
-        document.getElementById("start").textContent = "Start"
-        //Timer starts
-        
-    console.log(selectedCombination.message);
-    let startingMinutes = selectedCombination.message;
-    let time = startingMinutes * 60;
-
     const countdownElement = document.getElementById("timer");
 
-    function updateCountdown() {
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
+    if (!isTimerRunning) {
+        // Timer starts or resumes
+        document.getElementById("start").textContent = "Stop";
+        isTimerRunning = true;
 
-        countdownElement.textContent = `${minutes}: ${seconds}`;
-        time--;
-        document.getElementById("timer").textContent = time;
+        if (remainingTime === 0) {
+            // If no remaining time, use the initial starting time
+            startingMinutes = selectedCombination.message;
+            remainingTime = startingMinutes * 60;
+        }
+
+        function updateCountdown() {
+            const minutes = Math.floor(remainingTime / 60);
+            let seconds = remainingTime % 60;
+
+            seconds = seconds < 10 ? `0` + seconds : seconds;
+
+            countdownElement.textContent = `${minutes}:${seconds}`;
+            remainingTime--;
+
+            if (remainingTime < 0) {
+                clearInterval(timerInterval); // Clear the interval when time reaches 0
+                document.getElementById("start").textContent = "Start";
+                isTimerRunning = false;
+            }
+        }
+
+        updateCountdown(); // Initial call to display the time immediately
+        timerInterval = setInterval(updateCountdown, 1000);
+    } else {
+        // Timer pauses
+        document.getElementById("start").textContent = "Start";
+        clearInterval(timerInterval); // Clear the interval when pausing
+        isTimerRunning = false;
     }
-
-    setInterval(updateCountdown, 1000);
-    }
-
 }
