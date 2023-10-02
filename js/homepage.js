98cb166438dc88adad94d9e6f9984b401dd82d7c
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-10-02 12:01:16 +0200

    Update in homepage.js and changes filenames

---
+++ js/homepage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function renderHomePage () {
+
+    document.querySelector("main").innerHTML = `
+    
+    <div id="homePage">
+        <h3>Let's get cooking!</h3>
+        <p>Put your egg in after the water starts boiling!"
+    </div>
+    <button id=next>Next</button>
+    `;
+
+    document.getElementById("next").addEventListener("click", render)
+
+}
